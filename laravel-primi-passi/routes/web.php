6c169a3fd7ff1a4892a88426e319500033ac53ad
ElisaMavilia <elisamavilia1@gmail.com>
2024-05-17 16:17:10 +0200

    added contact.blade

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', 
+    ['name' => 'Elisa', 'surname' => 'Mavilia']);   
+});
+
+Route::get('/contatti', function () {
+    return view('contacts');
 });
